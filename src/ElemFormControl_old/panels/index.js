b89dcae6bca8c16196355ceebc7fc58744ee0d63
Egor Nikonorov <109210275+EgorAlifa@users.noreply.github.com>
2026-03-25 14:13:11 +0300

    Delete src/ElemFormControl_old directory

--- src/ElemFormControl_old/panels/index.js
+++
@@ -1,5 +0,0 @@
-import { useStyleConditionsPanel, useActionsPanel } from '../../shared';
-
-export const SettingsPanelAsync = () => import('./SettingsPanel.vue');
-
-export default [SettingsPanelAsync, useStyleConditionsPanel(), useActionsPanel()];
