4bc0cedb0c8db382c2aa229f4a716f4300ded480
Egor Nikonorov <109210275+EgorAlifa@users.noreply.github.com>
2026-04-17 18:43:47 +0300

    Delete src/ElemPivotTable directory

--- src/ElemPivotTable/workers/index.js
+++
@@ -1,4 +0,0 @@
-export { asyncGenerateTableMaps } from './generateTableMaps';
-export { asyncGenerateTableCells } from './generateTableCells';
-export { asyncGenerateHtmlTable } from './generateHtmlTable';
-export { asyncGenerateFlatTable } from './generateFlatTable';
