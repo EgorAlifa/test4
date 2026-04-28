10d8bc804301bfee6c3688b55761f7884c380052
Egor Nikonorov <109210275+EgorAlifa@users.noreply.github.com>
2026-04-28 11:37:32 +0300

    Delete src/ElemSmartGallery_old directory

--- src/ElemSmartGallery_old/panels/index.js
+++
@@ -1,8 +0,0 @@
-import { DremioPanelAsync } from '@goodt-common/dremio-panels';
-
-const DremioAddonsPanelAsync = () => import('./DremioAddonsPanel.vue');
-const GridPanelAsync = () => import('./GridPanel.vue');
-const MetricsPanelAsync = () => import('./MetricsPanel.vue');
-const SlotsPanelAsync = () => import('./SlotsPanel.vue');
-
-export default [DremioPanelAsync, DremioAddonsPanelAsync, MetricsPanelAsync, SlotsPanelAsync, GridPanelAsync];
