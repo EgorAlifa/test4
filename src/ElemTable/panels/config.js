2f43c51590d758d23ef29b2ed5203127e7d313b2
Egor Nikonorov <109210275+EgorAlifa@users.noreply.github.com>
2026-05-07 16:16:19 +0300

    Delete src/ElemTable directory

--- src/ElemTable/panels/config.js
+++
@@ -1,6 +0,0 @@
-export const SizeOptions = [{ value: null, label: 'inherit' }];
-
-export const ResultRowPositionOptions = [
-    { value: 'top', label: 'Сверху' },
-    { value: 'bottom', label: 'Снизу' }
-];
