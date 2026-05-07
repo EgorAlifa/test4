b472f6a048f80afe030f277d4a447942a6171ce3
Egor Nikonorov <109210275+EgorAlifa@users.noreply.github.com>
2026-05-07 16:16:26 +0300

    Delete src/ElemRow directory

--- src/ElemRow/panels/index.js
+++
@@ -1,5 +0,0 @@
-import { useActionsPanel, useStyleConditionsPanel } from '@goodt-widgets-insight/table-common';
-
-export const SettingsPanelAsync = () => import('./SettingsPanel.vue');
-
-export default [SettingsPanelAsync, useActionsPanel(), useStyleConditionsPanel()];
