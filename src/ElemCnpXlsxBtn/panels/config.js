cc5663cab2cf14b4d8078ae7c0321d14d3e9fdd8
Egor Nikonorov <109210275+EgorAlifa@users.noreply.github.com>
2026-06-08 12:34:27 +0300

    Delete src/ElemCnpXlsxBtn directory

--- src/ElemCnpXlsxBtn/panels/config.js
+++
@@ -1,6 +0,0 @@
-import { BookType } from '../constants';
-
-export const BookTypeOptions = [
-    { label: BookType.XLSX, value: BookType.XLSX },
-    { label: BookType.CSV, value: BookType.CSV }
-];
--- src/ElemCnpXlsxBtn/panels/index.js
+++
@@ -1,6 +0,0 @@
-const SettingsPanelAsync = () => import('./SettingsPanel.vue');
-const BtnPanelAsync = () => import('./BtnPanel.vue');
-const ColsPanelAsync = () => import('./ColsPanel.vue');
-const DebugPanelAsync = () => import('./DebugPanel.vue');
-
-export default [SettingsPanelAsync, BtnPanelAsync, ColsPanelAsync, DebugPanelAsync];
